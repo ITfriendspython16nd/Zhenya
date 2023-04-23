303b9ac4670a4eeb7e9af577e392ba7bd0ae805f
ITfriendspython16nd <128988120+ITfriendspython16nd@users.noreply.github.com>
2023-04-23 17:55:50 +0300

    Add files via upload

--- Site2/blog/models.py
+++ Site2/blog/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 
-# Create your models here
+# Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    image = models.ImageField(upload_to='photos/%Y/%m/%d', null=True, blank=True)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+     title = models.CharField(max_length=255)
+     text = models.TextField()
+     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+     # photo = models.ImageField(upload_to="photos/%Y/%m/%d")
 
-    def __str__(self):
-        return self.title
-    
+     def __str__(self):
+          return self.title
+
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
+     def __str__(self):
+          return self.name
--- Site2/blog/views.py
+++ Site2/blog/views.py
@@ -1,26 +1,45 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, View
 from blog.models import Post
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
-
-
-def view_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_vaild():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'index.html', {"posts": posts})
+     posts = Post.objects.all()
+     paginator = Paginator(posts, 3)
+     page = request.GET.get('page')
+     return render(request, 'index.html', {"posts": posts})
+def detail(request, pk):
+     post = Post.objects.get(id=pk)
+     return render(request, 'detail.html', {"post": post})
 
-def detail(request, id):
-    return render(request, 'detail.html', {})
+def view_Login(request):
+     if request.method =='POST':
+          form = AuthenticationForm(data=request.POST)
+          if form.is_valid():
+               username = form.cleaned_data('username')
+               password = form.cleaned_data('password')
+               user = authenticate(username = username, password = password)
+               if user is not None:
+                    login(request, user)
+                    return redirect('home')
 
+def logout_view(request):
+     logout(request)
+     return redirect(login)
 
+class Register(View):
+     form_class = UserCreationForm
+     template_name = 'register,html'
+
+     def get(self, request):
+          form = self.form_class()
+          return render(request, self.template_name, {'form': form})
+
+     def post(self, request):
+          form = self.form_class(request.POST)
+          if form.is_valid():
+               user = form.save()
+               return redirect('login')
+          return render(request, self.template_name, {'form': form})
